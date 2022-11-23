edc7bbe97baa334556322186aa8e7559c7215e87
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 13:34:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class PrimeNumber
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        Console.WriteLine("Enter a value: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 2)
+        {
+            Console.WriteLine("Prime Number");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Not Prime Number");
+            Console.ReadLine();
+        }
+    }
+
+}
