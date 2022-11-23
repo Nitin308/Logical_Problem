2521f01bf0accb2e35f434a538763a8e4fe393eb
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 13:39:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class StopwatchProgram
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        Console.WriteLine("Start");
+        stopwatch.Start();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(20);
+        }
+
+        Console.WriteLine("Stop");
+        stopwatch.Stop();
+
+        Console.WriteLine("Time Elapsed: {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+    }
+}
