303f1631004459067df4553185afac22165aa83f
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 13:36:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class ReverseNumber
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number to reverse: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = 0;
+        int remainder;
+
+        while (n != 0)
+        {
+            remainder = n % 10;
+            result = (result * 10) + remainder;
+            n = n / 10;  //quotient
+        }
+        Console.WriteLine("The reverse is: " + result);
+        Console.ReadLine();
+    }
+}
