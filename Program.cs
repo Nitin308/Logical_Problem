fcf7de9415006053286000b75d86c2b2ea7d08a8
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 13:29:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class FinonacciSeries
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter N terms for the Fibonaccis Series: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        Console.WriteLine("The Fibonacci Series for {0} terms are: ", N);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        for (int i = 0; i < (N - 2); i++)
+        {
+            c = a + b;
+            Console.WriteLine(c);
+            a = b;
+            b = c;
+        }
+        Console.ReadLine();
+    }
+}
