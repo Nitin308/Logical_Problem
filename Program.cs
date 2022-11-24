b5474d77ffbddf3de5c7988ba66cd41f4503f2d4
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-24 12:39:43 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class PerfectNumber
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        Console.WriteLine("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int n = num;
+
+        for (int i = 1; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == n)
+        {
+            Console.WriteLine("{0} is a Perfect Number", num);
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a Perfect Number", num);
+            Console.ReadLine();
+        }
+    }
+        
+}
